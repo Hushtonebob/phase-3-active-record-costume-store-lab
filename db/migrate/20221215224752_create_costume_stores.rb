281e39931a396e8e34d6e239b6d94a06a28329ed
Bobby De Luna <bobby.deluna2@gmail.com>
2023-01-04 16:18:14 -0700

    commit tree 4227e0abc69eeb2bda55cd3061b8705ad84b2734

---
+++ db/migrate/20221215224752_create_costume_stores.rb
@@ -0,0 +1,14 @@
+class CreateCostumeStores < ActiveRecord::Migration[6.1]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :is_in_buisness
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.text :description
+    end
+  end
+end
